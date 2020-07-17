299dc7b022bef33d6b615c07d6e7e564890e4e97
LeoCR <leonardoas2007@gmail.com>
2020-07-17 17:14:26 -0600

    Migrating to functional Components

--- src/components/content/about/TitleAbout.js
+++ src/components/content/about/TitleAbout.js
@@ -1,50 +1,49 @@
 import React from 'react';  
-export default class TitleAbout extends React.Component   {  
-    render(){
-        return(
-            <svg id="svg_bg_title_about_us" viewBox="0 0 903 466" version="1.1" 
-                xmlns="http://www.w3.org/2000/svg"  
-                style={{fillRule:'evenodd',clipRule:'evenodd',
-                width:'70%',height:'100%',position:'relative',float:'right',
-                strokeMiterlimit:1.5}}>
-                <g id="bg_title_about_us" transform="matrix(1.00057,0,0,0.975726,6.74318,8.90645)">
-                    <rect x="-6.739" y="-9.128" width="902.311" height="477.229" style={{fill:'none'}}/>
-                    <clipPath id="bg_title_about_us_clip">
-                        <rect x="-6.739" y="-9.128" width="902.311" height="477.229"/>
-                    </clipPath>
-                    <g clipPath="url(#bg_title_about_us_clip)">
-                        <g transform="matrix(0.999431,0,0,1.02488,-9.79489,-51.2281)">
-                            <path d="M1.177,515.504L1.177,388.163L94.82,265.402L191.594,515.504L1.177,515.504Z" style={{stroke:'black',strokeWidth:'1px'}}/>
-                        </g>
-                        <g transform="matrix(0.999431,0,0,1.02488,-9.79489,-51.2281)">
-                            <path d="M147.127,518.131L555.838,507.276L111.675,305.266L147.127,518.131Z" style={{stroke:'black',strokeWidth:'1px'}}/>
-                        </g>
-                        <g transform="matrix(0.999431,0,0,1.02488,-9.79489,-51.2281)">
-                            <path d="M94.976,265.558L109.031,310.701L553.681,513.771L579.07,125.097L94.976,265.558Z" style={{stroke:'black',strokeWidth:'1px'}}/>
-                        </g>
-                        <g transform="matrix(0.999431,0,0,1.02488,-9.79489,-51.2281)">
-                            <path d="M673.897,516.483L543.363,516.483L578.947,125.111L813.863,82.539L673.897,516.483Z" style={{stroke:'black',strokeWidth:'1px'}}/>
-                        </g>
-                        <g transform="matrix(0.999431,0,0,1.02488,-9.79489,-51.2281)">
-                            <path d="M668.125,510.219L826.356,510.219L773.515,296.848L668.125,510.219Z" style={{stroke:'black',strokeWidth:'1px'}}/>
-                        </g>
-                        <g transform="matrix(0.999431,0,0,1.02488,-9.79489,-51.2281)">
-                            <path d="M811.844,83.116L914.058,73.201L914.058,512.756L825.087,512.756L773.644,301.088L667.804,512.756L811.844,83.116Z" style={{stroke:'black',strokeWidth:'1px'}}/>
-                        </g>
-                        <g transform="matrix(1.41804,0,0,1.45414,-34.0232,-158.377)">
-                            <text id="txt_title_about_us_clip" x="236.338px"
-                                 y="335.752px" 
-                                 style={{
-                                        fontSize:'64px',
-                                        fill:'white',stroke:'white',strokeWidth:'0.7px',
-                                        strokeLinejoin:'round',strokeMiterlimit:1.41421
-                                }}>
-                            ABOUT ME
-                            </text>
-                        </g>
+export const TitleAbout =()=>   {  
+    return(
+        <svg id="svg_bg_title_about_us" viewBox="0 0 903 466" version="1.1" 
+            xmlns="http://www.w3.org/2000/svg"  
+            style={{fillRule:'evenodd',clipRule:'evenodd',
+            width:'70%',height:'100%',position:'relative',float:'right',
+            strokeMiterlimit:1.5}}>
+            <g id="bg_title_about_us" transform="matrix(1.00057,0,0,0.975726,6.74318,8.90645)">
+                <rect x="-6.739" y="-9.128" width="902.311" height="477.229" style={{fill:'none'}}/>
+                <clipPath id="bg_title_about_us_clip">
+                    <rect x="-6.739" y="-9.128" width="902.311" height="477.229"/>
+                </clipPath>
+                <g clipPath="url(#bg_title_about_us_clip)">
+                    <g transform="matrix(0.999431,0,0,1.02488,-9.79489,-51.2281)">
+                        <path d="M1.177,515.504L1.177,388.163L94.82,265.402L191.594,515.504L1.177,515.504Z" style={{stroke:'black',strokeWidth:'1px'}}/>
+                    </g>
+                    <g transform="matrix(0.999431,0,0,1.02488,-9.79489,-51.2281)">
+                        <path d="M147.127,518.131L555.838,507.276L111.675,305.266L147.127,518.131Z" style={{stroke:'black',strokeWidth:'1px'}}/>
+                    </g>
+                    <g transform="matrix(0.999431,0,0,1.02488,-9.79489,-51.2281)">
+                        <path d="M94.976,265.558L109.031,310.701L553.681,513.771L579.07,125.097L94.976,265.558Z" style={{stroke:'black',strokeWidth:'1px'}}/>
+                    </g>
+                    <g transform="matrix(0.999431,0,0,1.02488,-9.79489,-51.2281)">
+                        <path d="M673.897,516.483L543.363,516.483L578.947,125.111L813.863,82.539L673.897,516.483Z" style={{stroke:'black',strokeWidth:'1px'}}/>
+                    </g>
+                    <g transform="matrix(0.999431,0,0,1.02488,-9.79489,-51.2281)">
+                        <path d="M668.125,510.219L826.356,510.219L773.515,296.848L668.125,510.219Z" style={{stroke:'black',strokeWidth:'1px'}}/>
+                    </g>
+                    <g transform="matrix(0.999431,0,0,1.02488,-9.79489,-51.2281)">
+                        <path d="M811.844,83.116L914.058,73.201L914.058,512.756L825.087,512.756L773.644,301.088L667.804,512.756L811.844,83.116Z" style={{stroke:'black',strokeWidth:'1px'}}/>
+                    </g>
+                    <g transform="matrix(1.41804,0,0,1.45414,-34.0232,-158.377)">
+                        <text id="txt_title_about_us_clip" x="236.338px"
+                                y="335.752px" 
+                                style={{
+                                    fontSize:'64px',
+                                    fill:'white',stroke:'white',strokeWidth:'0.7px',
+                                    strokeLinejoin:'round',strokeMiterlimit:1.41421
+                            }}>
+                        ABOUT ME
+                        </text>
                     </g>
                 </g>
-            </svg>
-        );
-    }
+            </g>
+        </svg>
+    ); 
 }
+export default TitleAbout;
